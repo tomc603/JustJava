f21ebe66e00a039de0959a443643394d969a7ad6
Tom Cameron <tomc603@gmail.com>
2015-09-13 14:08:28 -0400

    Logic, comment, and feature updates
    Comment remaining methods
    Notify user with toasts
    Add a maximum to the coffee order
    Add a method to reset our state after an order is placed
    Add a method to execute orders to get the complexity out of the button click event.
    Use a string formatter wherever possible to make sure we use local formatting rules.

--- app/src/main/java/com/postboxdinosaur/android/justjava/MainActivity.java
+++ app/src/main/java/com/postboxdinosaur/android/justjava/MainActivity.java
@@ -4,58 +4,127 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    final double coffeePrice = 2.75;
+    boolean quantityAlert = false;
     int coffeeCount = 0;
-    double coffeePrice = 2.75;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        displayQuantity();
+        quantityReset();
     }
 
     /**
      * This method is called when the Order button is clicked
      */
     public void orderClicked(View view) {
-//        displayQuantity(1);
-//        displayPrice();
+        orderPlace();
     }
 
+    /**
+     * Place the coffee order and give the user an order number.
+     * <p/>
+     * Reset the display after the order is placed.
+     */
+    public void orderPlace() {
+        Random r = new Random();
+
+        displayMessage(String.format(Locale.getDefault(), "Thanks! Order Number %d.", r.nextInt()));
+        quantityReset();
+    }
+
+    /**
+     * This method resets the order quantity, the display, and the alert for orders over 25 coffees
+     */
+    public void quantityReset() {
+        coffeeCount = 0;
+        quantityAlert = false;
+        displayQuantity();
+    }
+
+    /**
+     * This method increases the coffee quantity by 1
+     */
     public void quantityIncrement(View view) {
         updateQuantity(1);
     }
 
+    /**
+     * This method decreases the coffee quantity by 1
+     */
     public void quantityDecrement(View view) {
         updateQuantity(-1);
     }
 
+    /**
+     * Increases or decreases coffee count, checking for minimum (0) and maximum (100)
+     *
+     * The user is alerted when they attempt to order more than the maximum, and they're
+     * given a special message when they increase their order to more than 25.
+     *
+     * @param number Integer to add to coffeeCount
+     */
     private void updateQuantity(int number) {
         coffeeCount += number;
         if (coffeeCount < 0) {
             coffeeCount = 0;
+        } else if ((coffeeCount > 25) && (!quantityAlert)) {
+            displayMessage("That's a lot of coffee!");
+            quantityAlert = true;
+        } else if (coffeeCount > 100) {
+            coffeeCount = 100;
+            displayMessage("Maximum coffees");
         }
         displayQuantity();
-        displayPrice();
     }
 
+    /**
+     * Calculates the total cost of the order based on the current quantity.
+     *
+     * @return Total cost
+     */
+    private double calculateTotal() {
+        return coffeeCount * coffeePrice;
+    }
+
+    /**
+     * Updates the quantity TextView and calls displayPrice() to update the price TextView
+     */
     private void displayQuantity() {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
 
-        quantityTextView.setText("" + coffeeCount);
+        quantityTextView.setText(String.format(Locale.getDefault(), "%d", coffeeCount));
         displayPrice();
     }
 
+    /**
+     * Updates the price TextView, formatting the price in the local currency.
+     *
+     * Price is calculated by the calculateTotal() method.
+     */
     private void displayPrice() {
         TextView priceTextView = (TextView) findViewById(
                 R.id.price_text_view);
 
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(coffeePrice * coffeeCount));
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(calculateTotal()));
+    }
+
+    /**
+     * Displays a Toast to the user
+     *
+     * @param message Text of the Toast
+     */
+    private void displayMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
